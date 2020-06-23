2699eb58d8b52b726bdbfbcaa8d224d9f054918b
hui liang <henry_liang71@hotmail.com>
2020-06-23 00:35:31 -0700

    restore original setting

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -175,11 +175,11 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.innerLoopGain = 5.5
       ret.lateralTuning.indi.outerLoopGain = 4.5
       ret.lateralTuning.indi.timeConstant = 1.0
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.8
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.76
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
       ret.wheelbase = 2.7
-      ret.steerRatio = 20  #Spec 13.73
+      ret.steerRatio = 24  #Spec 13.73
       tire_stiffness_factor = 0.385
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
