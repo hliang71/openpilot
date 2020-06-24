373b6e0133cf020d98e26295a92e92d1369e6a69
hui liang <henry_liang71@hotmail.com>
2020-06-23 19:42:33 -0700

    more tunes

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -172,9 +172,9 @@ class CarInterface(CarInterfaceBase):
       ret.minSteerSpeed = 0.
     elif candidate in [CAR.KIA_NIRO_HEV, CAR.KIA_NIRO_EV]:
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 5.4
+      ret.lateralTuning.indi.innerLoopGain = 5.6
       ret.lateralTuning.indi.outerLoopGain = 4.6
-      ret.lateralTuning.indi.timeConstant = 0.7
+      ret.lateralTuning.indi.timeConstant = 1.2
       ret.lateralTuning.indi.actuatorEffectiveness = 1.76
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
