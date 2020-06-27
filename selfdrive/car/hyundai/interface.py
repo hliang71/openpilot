9e2385b846f741ad6edf0beac666348077e08243
hui liang <henry_liang71@hotmail.com>
2020-06-27 15:08:16 -0700

    more tune

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -175,7 +175,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.innerLoopGain = 5
       ret.lateralTuning.indi.outerLoopGain = 4.6
       ret.lateralTuning.indi.timeConstant = 1.45
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.25
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.3
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
       ret.wheelbase = 2.7
