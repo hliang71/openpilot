980900f4eff98956add3a805089408006e29ee45
hui liang <henry_liang71@hotmail.com>
2020-06-28 13:52:40 -0700

    restore last night's setting

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -173,13 +173,13 @@ class CarInterface(CarInterfaceBase):
     elif candidate in [CAR.KIA_NIRO_HEV, CAR.KIA_NIRO_EV]:
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGain = 5
-      ret.lateralTuning.indi.outerLoopGain = 4.5
-      ret.lateralTuning.indi.timeConstant = 1.28
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.3
+      ret.lateralTuning.indi.outerLoopGain = 4
+      ret.lateralTuning.indi.timeConstant = 1.2
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.45
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
       ret.wheelbase = 2.7
-      ret.steerRatio = 24  #Spec 13.73
+      ret.steerRatio = 20  #Spec 13.73
       tire_stiffness_factor = 0.385
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
