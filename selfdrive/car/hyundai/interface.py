5275fd62a43d57761079ab4093f67f2eaee6560e
hui liang <henry_liang71@hotmail.com>
2020-06-27 20:30:55 -0700

    adjust more based on new torque

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -174,12 +174,12 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGain = 5
       ret.lateralTuning.indi.outerLoopGain = 4.5
-      ret.lateralTuning.indi.timeConstant = 1.28
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.3
+      ret.lateralTuning.indi.timeConstant = 1.25
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.35
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
       ret.wheelbase = 2.7
-      ret.steerRatio = 24  #Spec 13.73
+      ret.steerRatio = 20  #Spec 13.73
       tire_stiffness_factor = 0.385
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
