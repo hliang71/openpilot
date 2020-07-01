8fc3e709fbdb60756c3e1742fec252b41880f677
hui liang <henry_liang71@hotmail.com>
2020-07-01 14:00:54 -0700

    more tune

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -179,9 +179,9 @@ class CarInterface(CarInterfaceBase):
       #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
       ret.wheelbase = 2.7
-      ret.steerRatio = 18  #Spec 13.73
+      ret.steerRatio = 20  #Spec 13.73
       tire_stiffness_factor = 0.385
-      ret.steerActuatorDelay = 0.15
+      ret.steerActuatorDelay = 0.25
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate in [CAR.GRANDEUR, CAR.GRANDEUR_HEV]:
