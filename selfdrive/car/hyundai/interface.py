4f93dd50021ad9d632846b78e39c4f58b09f0acb
hui liang <henry_liang71@hotmail.com>
2020-07-01 12:14:37 -0700

    increase Actuator responsiveness

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -181,6 +181,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.7
       ret.steerRatio = 18  #Spec 13.73
       tire_stiffness_factor = 0.385
+      ret.steerActuatorDelay = 0.08
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate in [CAR.GRANDEUR, CAR.GRANDEUR_HEV]:
