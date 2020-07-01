b56b21c8f83127e00474e479eb2b5532cf6a51d5
hui liang <henry_liang71@hotmail.com>
2020-07-01 16:11:40 -0700

    modified long tuning to increase braking or acc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -217,7 +217,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerControlType = car.CarParams.SteerControlType.torque
 
     ret.longitudinalTuning.kpBP = [0., 10., 40.]
-    ret.longitudinalTuning.kpV = [1.2, 0.6, 0.2]
+    ret.longitudinalTuning.kpV = [1.2, 0.7, 0.4] #[1.2, 0.6, 0.2]
     ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
     ret.longitudinalTuning.kiV = [0.05, 0.02, 0.01, 0.005]
     ret.longitudinalTuning.deadzoneBP = [0., 40]
