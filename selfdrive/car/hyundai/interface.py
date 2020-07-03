eec5733855b621eaa6a3315b5c18f30fce029171
hui liang <henry_liang71@hotmail.com>
2020-07-03 16:11:48 -0700

    lower delay

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -181,7 +181,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.7
       ret.steerRatio = 24  #Spec 13.73
       tire_stiffness_factor = 0.385
-      ret.steerActuatorDelay = 0.3 #0.3
+      ret.steerActuatorDelay = 0.26 #0.3
       ret.steerLimitTimer = 0.1 #0.8
       #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
