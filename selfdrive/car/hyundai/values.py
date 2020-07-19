12991a0475a8116ca4c62fb127918f0bf0351cf9
hui liang <henry_liang71@hotmail.com>
2020-07-19 03:09:27 -0700

    increase max steer torque to 600

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -6,9 +6,9 @@ Ecu = car.CarParams.Ecu
 
 # Steer torque limits
 class SteerLimitParams:
-  STEER_MAX = 460   # 409 is the max, 255 is stock
-  STEER_DELTA_UP = 6
-  STEER_DELTA_DOWN = 8
+  STEER_MAX = 600   # 409 is the max, 255 is stock
+  STEER_DELTA_UP = 5
+  STEER_DELTA_DOWN = 7
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
