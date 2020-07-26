0ea4ef7086a327ef72d98134e2351a908e7f1677
hui liang <henry_liang71@hotmail.com>
2020-07-25 20:40:17 -0700

    put in my own setting

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -173,23 +173,21 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 0.
-    elif candidate in [CAR.KIA_NIRO_HEV, CAR.KIA_NIRO_EV]:
-      if candidate == CAR.KIA_NIRO_EV:
-        ret.lateralTuning.init('indi')
-        ret.lateralTuning.indi.innerLoopGain = 5.5
-        ret.lateralTuning.indi.outerLoopGain = 4.5
-        ret.lateralTuning.indi.timeConstant = 1.0
-        ret.lateralTuning.indi.actuatorEffectiveness = 1.76
-        ret.steerRatio = 24
-      else:
-        ret.steerRatio = 13.73
-        ret.lateralTuning.pid.kf = 0.00005
-        ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+    elif candidate == CAR.KIA_NIRO_EV:
+      ret.lateralTuning.init('indi')
+      ret.lateralTuning.indi.innerLoopGain = 5
+      ret.lateralTuning.indi.outerLoopGain = 4.85  # 4.8
+      ret.lateralTuning.indi.timeConstant = 2.088  # 1.16 x 1.8
+      ret.lateralTuning.indi.actuatorEffectiveness = 2.034  # 1.13 x 1.8
+      # ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1737. + STD_CARGO_KG
       ret.wheelbase = 2.7
-         #Spec
+      ret.steerRatio = 13.75  # Spec 13.73
       tire_stiffness_factor = 0.385
+      ret.steerActuatorDelay = 0.255  # 0.28  # 0.3
+      ret.steerLimitTimer = 0.4  # 0.8
+      # ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
+      # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate in [CAR.GRANDEUR, CAR.GRANDEUR_HEV]:
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1719. + STD_CARGO_KG
