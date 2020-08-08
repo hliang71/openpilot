8017f282d13c49d6913153bd80a3290733cd15d4
hui liang <henry_liang71@hotmail.com>
2020-08-07 21:37:34 -0700

    roll back

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -120,7 +120,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate == CAR.KIA_NIRO_EV:
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 5.0 #2.3595#2.3195#5 #1.75 #1.8468                                       #2.3085 #2.565 #2.85 #4.73 #5
+      ret.lateralTuning.indi.innerLoopGain = 2.3595 #2.3595#2.3195#5 #1.75 #1.8468                                     #2.3085 #2.565 #2.85 #4.73 #5
       ret.lateralTuning.indi.outerLoopGain = 2.268                #4.8 #1.72 #1.8144                                   #2.268 #2.52 #2.8 #4.6 #4.85  # 4.8
       ret.lateralTuning.indi.timeConstant = 1.392                 #1.16 x 1.83#1.16x0.8424#1.16 x 0.936                #1.16 x 1.2#1.16x1.4#1.16 x 1.7# 1.16 x 1.8
       ret.lateralTuning.indi.actuatorEffectiveness = 1.582        #1.13 x 1.83#1.13x0.8424#1.13x0.936                  #1.13x1.2#1.13x1.4#1.13 x 1.7 # 1.13 x 1.8
